da64e3c21461075d360d1a46c77ea4ea143990ac
René Blažek <blazek.rene@gmail.com>
2018-08-23 08:14:16 +0200

    Add files via upload

---
+++ Extensions.swift
@@ -0,0 +1,594 @@
+//
+//  Extensions.swift
+//  mostly used in Trenujme.sk App
+//
+//  Created by René Blažek on 6.10.17.
+//  Copyright © 2017 Originals,s.r.o. All rights reserved.
+//
+
+import UIKit
+import Foundation
+import SystemConfiguration
+
+extension Double {
+    var km: Double { return self * 1_000.0 }
+    var m: Double { return self }
+    var cm: Double { return self / 100.0 }
+    var mm: Double { return self / 1_000.0 }
+    var ft: Double { return self / 3.28084 }
+}
+
+struct Size {
+    var width = 0.0, height = 0.0
+}
+struct Point {
+    var x = 0.0, y = 0.0
+}
+struct Rect {
+    var origin = Point()
+    var size = Size()
+}
+
+extension Int {
+    func repetitions(task: () -> Void) {
+        for _ in 0..<self {
+            task()
+        }
+    }
+}
+
+extension DateFormatter {
+    
+    convenience init (format: String) {
+        self.init()
+        dateFormat = format
+        locale = Locale.current
+    }
+}
+
+extension Date {
+        func toString (format:String) -> String? {
+        return DateFormatter(format: format).string(from: self)
+    }
+
+    func stringToDate(_ str: String)->Date{
+        let formatter = DateFormatter()
+        formatter.dateFormat="yyyy-MM-dd hh:mm:ss Z"
+        return formatter.date(from: str)!
+    }
+
+    func toMillis() -> Int64! {
+        return Int64(self.timeIntervalSince1970)
+    }
+}
+
+extension String {
+    
+    func toDate (format: String) -> Date? {
+        return DateFormatter(format: format).date(from: self)
+    }
+    
+    func toDateString (inputFormat: String, outputFormat:String) -> String? {
+        if let date = toDate(format: inputFormat) {
+            return DateFormatter(format: outputFormat).string(from: date)
+        }
+        return nil
+    }
+    
+    func dateToString(_ str: Date)->String{
+        let formatter = DateFormatter()
+        formatter.timeStyle=DateFormatter.Style.short
+        return formatter.string(from: str)
+    }
+   
+    //To check text field or String is blank or not
+    var isBlank: Bool {
+        get {
+            let trimmed = trimmingCharacters(in: CharacterSet.whitespaces)
+            return trimmed.isEmpty
+        }
+    }
+    
+    //Validate Email
+    
+    var isEmail: Bool {
+        do {
+            let regex = try NSRegularExpression(pattern: "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,}", options: .caseInsensitive)
+            return regex.firstMatch(in: self, options: NSRegularExpression.MatchingOptions(rawValue: 0), range: NSMakeRange(0, self.characters.count)) != nil
+        } catch {
+            return false
+        }
+    }
+    
+    var isAlphanumeric: Bool {
+        return !isEmpty && range(of: "[^a-zA-Z0-9]", options: .regularExpression) == nil
+    }
+    
+    
+    //validate Password
+    var isValidPassword: Bool {
+        do {
+            let regex = try NSRegularExpression(pattern: "^[a-zA-Z_0-9\\-_,;.:#+*?=!§$%&/()@]+$", options: .caseInsensitive)
+            if(regex.firstMatch(in: self, options: NSRegularExpression.MatchingOptions(rawValue: 0), range: NSMakeRange(0, self.characters.count)) != nil){
+                
+                if(self.characters.count>=6 && self.characters.count<=20){
+                    return true
+                }else{
+                    return false
+                }
+            }else{
+                return false
+            }
+        } catch {
+            return false
+        }
+    }
+    
+    func isInt() -> Bool {
+        
+        if let intValue = Int(self) {
+            
+            if intValue >= 0 {
+                return true
+            }
+        }
+        
+        return false
+    }
+    
+    func isFloat() -> Bool {
+        
+        if let floatValue = Float(self) {
+            
+            if floatValue >= 0 {
+                return true
+            }
+        }
+        
+        return false
+    }
+    
+    func isDouble() -> Bool {
+        
+        if let doubleValue = Double(self) {
+            
+            if doubleValue >= 0 {
+                return true
+            }
+        }
+        
+        return false
+    }
+    
+    func toDouble() -> Double? {
+        let numberFormatter = NumberFormatter()
+        numberFormatter.locale = NSLocale(localeIdentifier: "en_US_POSIX") as Locale?
+        return numberFormatter.number(from: self)?.doubleValue
+    }
+    
+    func numberOfCharacters() -> Int {
+        return self.characters.count
+    }
+    
+}
+
+extension UITextField {
+    func setBottomBorderOnlyWith(color: CGColor) {
+        self.borderStyle = .none
+        self.layer.masksToBounds = false
+        self.layer.shadowColor = color
+        self.layer.shadowOffset = CGSize(width: 0.0, height: 1.0)
+        self.layer.shadowOpacity = 1.0
+        self.layer.shadowRadius = 0.0
+    }
+
+    func isError(baseColor: CGColor, numberOfShakes shakes: Float, revert: Bool) {
+        let animation: CABasicAnimation = CABasicAnimation(keyPath: "shadowColor")
+        animation.fromValue = baseColor
+        animation.toValue = UIColor.red.cgColor
+        animation.duration = 0.7
+        if revert { animation.autoreverses = true } else { animation.autoreverses = false }
+        self.layer.add(animation, forKey: "")
+        
+        let shake: CABasicAnimation = CABasicAnimation(keyPath: "position")
+        shake.duration = 0.07
+        shake.repeatCount = shakes
+        if revert { shake.autoreverses = true  } else { shake.autoreverses = false }
+        shake.fromValue = NSValue(cgPoint: CGPoint(x: self.center.x - 10, y: self.center.y))
+        shake.toValue = NSValue(cgPoint: CGPoint(x: self.center.x + 10, y: self.center.y))
+        self.layer.add(shake, forKey: "position")
+    }
+}
+
+
+extension NSAttributedString {
+    func stringFromHtml(string: String) -> NSAttributedString? {
+        let htmlData = NSString(string: string).data(using: String.Encoding.unicode.rawValue)
+        
+        let options = [NSAttributedString.DocumentReadingOptionKey.documentType: NSAttributedString.DocumentType.html]
+        
+        let attributedString = try! NSAttributedString(data: htmlData!, options: options, documentAttributes: nil)
+        return attributedString
+    }
+}
+
+
+/*
+ * Table handling extensions
+ */
+
+extension UITableView {
+    // setup empty message when table is empty
+    // use in tableView() as self.tableView.setEmptyMessage("my messsage") when count is == 0
+    func setEmptyMessage(_ message: String) {
+        let messageLabel = UILabel(frame: CGRect(x: 0, y: 0, width: self.bounds.size.width, height: self.bounds.size.height))
+        messageLabel.text = message
+        messageLabel.textColor = .black
+        messageLabel.numberOfLines = 0;
+        messageLabel.textAlignment = .center;
+        messageLabel.font = UIFont(name: "TrebuchetMS", size: 15)
+        messageLabel.sizeToFit()
+        
+        self.backgroundView = messageLabel;
+        self.separatorStyle = .none;
+    }
+    
+    // restore table
+    func restore() {
+        self.backgroundView = nil
+        self.separatorStyle = .singleLine
+    }
+}
+
+/*
+ * UIImage view handling downloads
+ * usage> imageView.downloadedFrom("url")
+ */
+
+extension UIImageView {
+    func downloadedFrom(url: URL, contentMode mode: UIViewContentMode = .scaleAspectFit) {
+        contentMode = mode
+        URLSession.shared.dataTask(with: url) { data, response, error in
+            guard
+                let httpURLResponse = response as? HTTPURLResponse, httpURLResponse.statusCode == 200,
+                let mimeType = response?.mimeType, mimeType.hasPrefix("image"),
+                let data = data, error == nil,
+                let image = UIImage(data: data)
+                else { return }
+            DispatchQueue.main.async() {
+                self.image = image
+            }
+            }.resume()
+    }
+    func downloadedFrom(link: String, contentMode mode: UIViewContentMode = .scaleAspectFit) {
+        guard let url = URL(string: link) else { return }
+        downloadedFrom(url: url, contentMode: mode)
+    }
+}
+
+/*
+ * UIView extensions
+ */
+extension UIView {
+    func fadeIn() {
+        // Move our fade out code from earlier
+        UIView.animate(withDuration: 1.0, delay: 0.0, options: UIViewAnimationOptions.curveEaseIn, animations: {
+            self.alpha = 1.0 // Instead of a specific instance of, say, birdTypeLabel, we simply set [thisInstance] (ie, self)'s alpha
+        }, completion: nil)
+    }
+    
+    func fadeOut() {
+        UIView.animate(withDuration: 1.0, delay: 0.0, options: UIViewAnimationOptions.curveEaseOut, animations: {
+            self.alpha = 0.0
+        }, completion: nil)
+    }
+}
+
+
+extension UIView {
+    
+    private struct AssociatedKeys {
+        static var descriptiveName = "AssociatedKeys.DescriptiveName.blurView"
+    }
+    
+    private (set) var blurView: BlurView {
+        get {
+            if let blurView = objc_getAssociatedObject(
+                self,
+                &AssociatedKeys.descriptiveName
+                ) as? BlurView {
+                return blurView
+            }
+            self.blurView = BlurView(to: self)
+            return self.blurView
+        }
+        set(blurView) {
+            objc_setAssociatedObject(
+                self,
+                &AssociatedKeys.descriptiveName,
+                blurView,
+                .OBJC_ASSOCIATION_RETAIN_NONATOMIC
+            )
+        }
+    }
+    
+    class BlurView {
+        
+        private var superview: UIView
+        private var blur: UIVisualEffectView?
+        private var editing: Bool = false
+        private (set) var blurContentView: UIView?
+        private (set) var vibrancyContentView: UIView?
+        
+        var animationDuration: TimeInterval = 0.1
+        
+        /**
+         * Blur style. After it is changed all subviews on
+         * blurContentView & vibrancyContentView will be deleted.
+         */
+        var style: UIBlurEffectStyle = .light {
+            didSet {
+                guard oldValue != style,
+                    !editing else { return }
+                applyBlurEffect()
+            }
+        }
+        /**
+         * Alpha component of view. It can be changed freely.
+         */
+        var alpha: CGFloat = 0 {
+            didSet {
+                guard !editing else { return }
+                if blur == nil {
+                    applyBlurEffect()
+                }
+                let alpha = self.alpha
+                UIView.animate(withDuration: animationDuration) {
+                    self.blur?.alpha = alpha
+                }
+            }
+        }
+        
+        init(to view: UIView) {
+            self.superview = view
+        }
+        
+        func setup(style: UIBlurEffectStyle, alpha: CGFloat) -> Self {
+            self.editing = true
+            
+            self.style = style
+            self.alpha = alpha
+            
+            self.editing = false
+            
+            return self
+        }
+        
+        func enable(isHidden: Bool = false) {
+            if blur == nil {
+                applyBlurEffect()
+            }
+            
+            self.blur?.isHidden = isHidden
+        }
+        
+        private func applyBlurEffect() {
+            blur?.removeFromSuperview()
+            
+            applyBlurEffect(
+                style: style,
+                blurAlpha: alpha
+            )
+        }
+        
+        private func applyBlurEffect(style: UIBlurEffectStyle,
+                                     blurAlpha: CGFloat) {
+            superview.backgroundColor = UIColor.clear
+            
+            let blurEffect = UIBlurEffect(style: style)
+            let blurEffectView = UIVisualEffectView(effect: blurEffect)
+            
+            let vibrancyEffect = UIVibrancyEffect(blurEffect: blurEffect)
+            let vibrancyView = UIVisualEffectView(effect: vibrancyEffect)
+            blurEffectView.contentView.addSubview(vibrancyView)
+            
+            blurEffectView.alpha = blurAlpha
+            
+            superview.insertSubview(blurEffectView, at: 0)
+            
+            blurEffectView.addAlignedConstrains()
+            vibrancyView.addAlignedConstrains()
+            
+            self.blur = blurEffectView
+            self.blurContentView = blurEffectView.contentView
+            self.vibrancyContentView = vibrancyView.contentView
+        }
+    }
+    
+    private func addAlignedConstrains() {
+        translatesAutoresizingMaskIntoConstraints = false
+        addAlignConstraintToSuperview(attribute: NSLayoutAttribute.top)
+        addAlignConstraintToSuperview(attribute: NSLayoutAttribute.leading)
+        addAlignConstraintToSuperview(attribute: NSLayoutAttribute.trailing)
+        addAlignConstraintToSuperview(attribute: NSLayoutAttribute.bottom)
+    }
+    
+    private func addAlignConstraintToSuperview(attribute: NSLayoutAttribute) {
+        superview?.addConstraint(
+            NSLayoutConstraint(
+                item: self,
+                attribute: attribute,
+                relatedBy: NSLayoutRelation.equal,
+                toItem: superview,
+                attribute: attribute,
+                multiplier: 1,
+                constant: 0
+            )
+        )
+    }
+}
+
+/*
+ * Internet connection checks
+ Usage: on viewDidLoad() currentReachabilityStatus != .notReacheble // returns true connected
+ */
+
+protocol Utilities {
+    
+}
+
+extension NSObject:Utilities{
+    
+    
+    enum ReachabilityStatus {
+        case notReachable
+        case reachableViaWWAN
+        case reachableViaWiFi
+    }
+    
+    var currentReachabilityStatus: ReachabilityStatus {
+        
+        var zeroAddress = sockaddr_in()
+        zeroAddress.sin_len = UInt8(MemoryLayout<sockaddr_in>.size)
+        zeroAddress.sin_family = sa_family_t(AF_INET)
+        
+        guard let defaultRouteReachability = withUnsafePointer(to: &zeroAddress, {
+            $0.withMemoryRebound(to: sockaddr.self, capacity: 1) {
+                SCNetworkReachabilityCreateWithAddress(nil, $0)
+            }
+        }) else {
+            return .notReachable
+        }
+        
+        var flags: SCNetworkReachabilityFlags = []
+        if !SCNetworkReachabilityGetFlags(defaultRouteReachability, &flags) {
+            return .notReachable
+        }
+        
+        if flags.contains(.reachable) == false {
+            // The target host is not reachable.
+            return .notReachable
+        }
+        else if flags.contains(.isWWAN) == true {
+            // WWAN connections are OK if the calling application is using the CFNetwork APIs.
+            return .reachableViaWWAN
+        }
+        else if flags.contains(.connectionRequired) == false {
+            // If the target host is reachable and no connection is required then we'll assume that you're on Wi-Fi...
+            return .reachableViaWiFi
+        }
+        else if (flags.contains(.connectionOnDemand) == true || flags.contains(.connectionOnTraffic) == true) && flags.contains(.interventionRequired) == false {
+            // The connection is on-demand (or on-traffic) if the calling application is using the CFSocketStream or higher APIs and no [user] intervention is needed
+            return .reachableViaWiFi
+        }
+        else {
+            return .notReachable
+        }
+    }
+    
+}
+
+extension String {
+    var htmlToAttributedString: NSAttributedString? {
+        guard let data = data(using: .utf8) else { return NSAttributedString() }
+        do {
+            return try NSAttributedString(data: data, options: [NSAttributedString.DocumentReadingOptionKey.documentType:  NSAttributedString.DocumentType.html], documentAttributes: nil)
+        } catch {
+            return NSAttributedString()
+        }
+    }
+    var htmlToString: String {
+        return htmlToAttributedString?.string ?? ""
+    }
+}
+
+extension UIColor {
+    convenience init(red: Int, green: Int, blue: Int) {
+        assert(red >= 0 && red <= 255, "Invalid red component")
+        assert(green >= 0 && green <= 255, "Invalid green component")
+        assert(blue >= 0 && blue <= 255, "Invalid blue component")
+        self.init(red: CGFloat(red) / 255.0, green: CGFloat(green) / 255.0, blue: CGFloat(blue) / 255.0, alpha: 1.0)
+    }
+    
+    convenience init(netHex:Int) {
+        self.init(red:(netHex >> 16) & 0xff, green:(netHex >> 8) & 0xff, blue:netHex & 0xff)
+    }
+}
+
+extension UIColor {
+    struct FlatColor {
+        struct Green {
+            static let Fern = UIColor(netHex: 0x6ABB72)
+            static let MountainMeadow = UIColor(netHex: 0x3ABB9D)
+            static let ChateauGreen = UIColor(netHex: 0x4DA664)
+            static let PersianGreen = UIColor(netHex: 0x2CA786)
+        }
+        
+        struct Blue {
+            static let PictonBlue = UIColor(netHex: 0x5CADCF)
+            static let Mariner = UIColor(netHex: 0x3585C5)
+            static let CuriousBlue = UIColor(netHex: 0x4590B6)
+            static let Denim = UIColor(netHex: 0x2F6CAD)
+            static let Chambray = UIColor(netHex: 0x485675)
+            static let BlueWhale = UIColor(netHex: 0x29334D)
+        }
+        
+        struct Violet {
+            static let Wisteria = UIColor(netHex: 0x9069B5)
+            static let BlueGem = UIColor(netHex: 0x533D7F)
+        }
+        
+        struct Yellow {
+            static let Energy = UIColor(netHex: 0xF2D46F)
+            static let Turbo = UIColor(netHex: 0xF7C23E)
+        }
+        
+        struct Orange {
+            static let NeonCarrot = UIColor(netHex: 0xF79E3D)
+            static let Sun = UIColor(netHex: 0xEE7841)
+        }
+        
+        struct Red {
+            static let TerraCotta = UIColor(netHex: 0xE66B5B)
+            static let Valencia = UIColor(netHex: 0xCC4846)
+            static let Cinnabar = UIColor(netHex: 0xDC5047)
+            static let WellRead = UIColor(netHex: 0xB33234)
+        }
+        
+        struct Gray {
+            static let AlmondFrost = UIColor(netHex: 0xA28F85)
+            static let WhiteSmoke = UIColor(netHex: 0xEFEFEF)
+            static let Iron = UIColor(netHex: 0xD1D5D8)
+            static let IronGray = UIColor(netHex: 0x75706B)
+        }
+    }
+}
+
+
+
+extension UIViewController {
+    func displayMsg(title : String?, msg : String,
+                    style: UIAlertControllerStyle = .alert,
+                    dontRemindKey : String? = nil) {
+        if dontRemindKey != nil,
+            UserDefaults.standard.bool(forKey: dontRemindKey!) == true {
+            return
+        }
+        
+        let ac = UIAlertController.init(title: title,
+                                        message: msg, preferredStyle: style)
+        ac.addAction(UIAlertAction.init(title: "OK",
+                                        style: .default, handler: nil))
+        
+        if dontRemindKey != nil {
+            ac.addAction(UIAlertAction.init(title: "Nezobrazovať znovu",
+                                            style: .default, handler: { (aa) in
+                                                UserDefaults.standard.set(true, forKey: dontRemindKey!)
+                                                UserDefaults.standard.synchronize()
+            }))
+        }
+        DispatchQueue.main.async {
+            self.present(ac, animated: true, completion: nil)
+        }
+    }
+}
